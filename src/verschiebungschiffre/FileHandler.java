677f0fd178431edcf2b0ff5928893f3a5ff57d87
Julian Von Mosig <julianvonmosig@gmail.com>
2023-05-17 11:54:03 +0200

    filereader explizit in utf-8 und .txt in utf-8 konvertiert

--- src/verschiebungschiffre/FileHandler.java
+++ src/verschiebungschiffre/FileHandler.java
@@ -1,45 +1,48 @@
 package verschiebungschiffre;
 
 import java.io.*;
+
+import java.nio.charset.Charset;
+
 /**
  * 
  * @author Simon Hoffmann
  *
  */
 public class FileHandler {
-	
+
 	private StringBuilder text;
+
 	/**
 	 * 
 	 */
 	public FileHandler() {
 		text = new StringBuilder();
 	}
+
 	/**
 	 * 
 	 * @param Dateiname
 	 */
-	public void readText(String filename){
-		try(
-		FileReader fr = new FileReader(filename);
-	    BufferedReader br = new BufferedReader(fr);
-				){
-	    String zeile = "";
+	public void readText(String filename) {
+		try (
+				FileReader fr = new FileReader(filename, Charset.forName("UTF-8"));
+				BufferedReader br = new BufferedReader(fr);) {
+			String zeile = "";
 
-	    do
-	    {
-	      zeile = br.readLine();
-	      text.append(zeile);
-	      text.append(" ");
-	    }
-	    while (zeile != null);
-	    br.close();
+			do {
+				zeile = br.readLine();
+				text.append(zeile);
+				text.append(" ");
+			} while (zeile != null);
+			br.close();
 		} catch (FileNotFoundException fe) {
 			fe.printStackTrace(System.err);
 		} catch (IOException ie) {
 			ie.printStackTrace(System.err);
 		}
 	}
+
 	/**
 	 * 
 	 * @param filename
@@ -48,19 +51,19 @@ public class FileHandler {
 	public void writeFile(String filename, String content) {
 		File datei = new File(filename);
 		try (
-			FileWriter fr = new FileWriter(datei);
-			BufferedWriter br = new BufferedWriter(fr);
-			PrintWriter pr = new PrintWriter(br);
-				){
+				FileWriter fr = new FileWriter(datei);
+				BufferedWriter br = new BufferedWriter(fr);
+				PrintWriter pr = new PrintWriter(br);) {
 			pr.println(content);
-			
+
 			pr.close();
-		}catch (FileNotFoundException fe) {
+		} catch (FileNotFoundException fe) {
 			System.out.println(fe);
-		}catch (IOException ie) {
+		} catch (IOException ie) {
 			System.out.println(ie);
 		}
 	}
+
 	/**
 	 * 
 	 */
--- src/verschiebungschiffre/Main.java
+++ src/verschiebungschiffre/Main.java
@@ -1,26 +1,24 @@
 package verschiebungschiffre;
 
-
 /**
  * 
- * @author Simon Hoffmann
+ * @author Simon Hoffmann, Julian Mosig von Aehrenfeld
  * @version 69.0
  */
 public class Main {
 	public static void main(String args[]) {
-		
-		//Könnte eine Lösungs fürs Problem mit dem BufferedReader sein!
-		//String fileName = ...; 
-		//String text = Files.readString(Path.of(fileName));
-		
-		
-		String dateipfad = System.getProperty("user.dir")+"\\" + "Gedicht.txt";
-		//System.getProperty("user.dir") --> direkt im aufgerufenen Verzeichnis
+
+		// Könnte eine Lösungs fürs Problem mit dem BufferedReader sein!
+		// String fileName = ...;
+		// String text = Files.readString(Path.of(fileName));
+
+		String dateipfad = System.getProperty("user.dir") + "\\" + "Gedicht.txt";
+		// System.getProperty("user.dir") --> direkt im aufgerufenen Verzeichnis
 		FileHandler text = new FileHandler();
 		text.readText(dateipfad);
 		String content = text.toString();
 		Chiffre c = new Chiffre();
 		String g = c.language(content);
-		System.out.println(g); //debug
+		System.out.println(g); // debug
 	}
 }
