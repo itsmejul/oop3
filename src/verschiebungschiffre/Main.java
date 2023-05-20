2cc6b9924ff0a5d7a799ba844821a820aed1241a
Simon <simihoff@gmx.de>
2023-05-20 22:08:13 +0200

    Das Programm bricht nach der Ausführung ab.

--- src/verschiebungschiffre/Main.java
+++ src/verschiebungschiffre/Main.java
@@ -17,31 +17,29 @@ public class Main {
 	 */
 	public static void main(String args[]) {
 		FileHandler fileHandler = new FileHandler();
-		while (true) {
-			String mode = UserInterface.in.requestChoiceName("\nChiffrieren oder Dechiffrieren?", "c", "d");
-			if (mode == "c") {
-				String fileName = UserInterface.in.requestString("Welche Datei soll gelesen werden?");
-				String filePath = System.getProperty("user.dir") + "\\" + fileName;
-				String resultFileName = UserInterface.in
-						.requestString("In welche Datei soll das Ergebnis geschrieben werden?");
+		String mode = UserInterface.in.requestChoiceName("\nChiffrieren oder Dechiffrieren?", "c", "d");
+		if (mode == "c") {
+			String fileName = UserInterface.in.requestString("Welche Datei soll gelesen werden?");
+			String filePath = System.getProperty("user.dir") + "\\" + fileName;
+			String resultFileName = UserInterface.in
+					.requestString("In welche Datei soll das Ergebnis geschrieben werden?");
 
-				int key = UserInterface.in.requestInt("Was ist der Schluessel?", 0, 25);
-				Chiffre c = new Chiffre(key, fileHandler, filePath, resultFileName);
-				c.fullTransformation();
+			int key = UserInterface.in.requestInt("Was ist der Schluessel?", 0, 25);
+			Chiffre c = new Chiffre(key, fileHandler, filePath, resultFileName);
+			c.fullTransformation();
+		} else {
+			String fileName = UserInterface.in.requestString("Welche Datei soll gelesen werden?");
+			String filePath = System.getProperty("user.dir") + "\\" + fileName;
+			String resultFileName = UserInterface.in
+					.requestString("In welche Datei soll das Ergebnis geschrieben werden?");
+			int key;
+			if (UserInterface.in.requestChoiceName("Hast du einen Schluessel?", "y", "n") == "y") {
+				key = UserInterface.in.requestInt("Was ist der Schluessel?", 0, 25);
 			} else {
-				String fileName = UserInterface.in.requestString("Welche Datei soll gelesen werden?");
-				String filePath = System.getProperty("user.dir") + "\\" + fileName;
-				String resultFileName = UserInterface.in
-						.requestString("In welche Datei soll das Ergebnis geschrieben werden?");
-				int key;
-				if (UserInterface.in.requestChoiceName("Hast du einen Schluessel?", "y", "n") == "y") {
-					key = UserInterface.in.requestInt("Was ist der Schluessel?", 0, 25);
-				} else {
-					key = -1;
-				}
-				Dechiffre dechiffre = new Dechiffre(key, fileHandler, filePath, resultFileName);
-				dechiffre.fullTransformation();
+				key = -1;
 			}
+			Dechiffre dechiffre = new Dechiffre(key, fileHandler, filePath, resultFileName);
+			dechiffre.fullTransformation();
 		}
 	}
 }
